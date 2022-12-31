3c4df8e7635331de749275ca152d2fd7444f3c25
kwakil1 <97229046+kwakil1@users.noreply.github.com>
2022-12-31 15:26:56 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+//write a function that takes 3 parameters (name, height, country)
+//write an alert in the function that prints out the inputs
+//call the function and pass the 3 parameters into it
+function myFunction(Name, Height, Country) {
+    console.log(Name + ',' + Height + ',' + Country);
+}
+
+myFunction('Kamilah W.', '62cm', 'USA');
+
